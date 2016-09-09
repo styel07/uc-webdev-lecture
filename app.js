b6c4da9aae280d2ff8599318630e669b23769e05
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2016-09-09 16:10:34 +0800

    included info on career, critical, developers tools

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ app.component('main', {
       <div class="row section">
         <discussion></discussion>
       </div>
-      <div class="row section" id="careerPath">
+      <div class="row section">
         <career-paths></career-paths>
       </div>
-      <div class="row section" id="checklist">
+      <div class="row section">
         <checklist></checklist>
       </div>
-      <div class="row section" id="devTools">
+      <div class="row section">
         <dev-tools></dev-tools>
       </div>
-      <div class="row section" id="careerAdvice">
+      <div class="row section">
         <career-advice></career-advice>
       </div>
-      <div class="row section" id="references">
+      <div class="row section">
         <references></references>
       </div>
     </div>
@@ -82,7 +82,7 @@ app.component('discussion', {
       this.greet = 'Discussion Overview';
   },
   template: `
-    <div class="jumbotron">
+    <div class="jumbotron" id="discussion">
       <div class="container">
         <h1>{{ $ctrl.greet }}</h1>
         <p>To have an open discussion on the latest trends technology and tools used in California</p>
@@ -96,16 +96,45 @@ app.component('careerPaths', {
       this.greet = 'Career Paths';
   },
   template: `
-      <div class="jumbotron">
+      <div class="jumbotron" id="careerPath">
         <div class="container">
           <h1>{{ $ctrl.greet }}</h1>
-          <ul>
-            <li>Front End</li>
-            <li>Backend</li>
-            <li>DevOps</li>
-          </ul>
+          <div class="col-md-4">
+            <div class="panel panel-danger">
+              <div class="panel-heading">
+                <h3 class="panel-title">Front End Developer</h3>
+              </div>
+              <div class="panel-body">
+                also known as "client-side development" is the practice of producing HTML, CSS and JavaScript for a website or web application so that a user can see and interact with them directly.
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="panel panel-success">
+              <div class="panel-heading">
+                <h3 class="panel-title">Front End Developer</h3>
+              </div>
+              <div class="panel-body">
+                also known as "server-side development" using code which connects with a database (using MySQL, SQL, Access etc.) to look up, save or change data and return it back to the user in form of front-end code.
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="panel panel-warning">
+              <div class="panel-heading">
+                <h3 class="panel-title">Development & Operations (DevOps)</h3>
+              </div>
+              <div class="panel-body">
+                The goal of DevOps is to change and improve the relationship by advocating better communication and collaboration between the two business units.
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
+
   `
 });
 
@@ -114,13 +143,36 @@ app.component('checklist', {
       this.greet = 'Critical Checklist';
   },
   template: `
-    <div class="jumbotron">
+    <div class="jumbotron" id="checklist">
       <div class="container">
         <h1>{{ $ctrl.greet }}</h1>
-          <ul>
-            <li>Portfolio (3-5 pieces)</li>
-            <li>Resume</li>
-          </ul>
+        <p class="white">Your Bread and Butter</p>
+
+        <div class="col-md-5 col-md-offset-1">
+          <div class="panel panel-success">
+            <div class="panel-heading">
+              <h3 class="panel-title">Portfolio</h3>
+            </div>
+            <div class="panel-body">
+            <ul>
+              <li>At least 3 or more apps</li>
+              <li>Apps should be Live</li>
+            </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-5">
+          <div class="panel panel-success">
+            <div class="panel-heading">
+              <h3 class="panel-title">Resume</h3>
+            </div>
+            <div class="panel-body">
+              This is a physical document or virtual portfolio of your professional history, projects, skillsets etc.
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   `
@@ -131,17 +183,76 @@ app.component('devTools', {
       this.greet = 'Developers Environment';
   },
   template: `
-    <div class="jumbotron">
+    <div class="jumbotron"  id="devTools">
       <div class="container">
         <h1>{{ $ctrl.greet }}</h1>
           <ul>
-            <li><a href="https://iterm2.com/">iterm2</a> - terminal tool</li>
-            <li>Brew - used to install node.js<a href="http://yoember.com/nodejs/the-best-way-to-install-node-js/"></a></li>
-            <li>Atom / Sublime - text editors<a href="https://atom.io"></a></li>
-            <li>Git<a href="https://git-for-windows.github.io/"></a></li>
-            <li>Github - repo<a href="https://github.com"></a></li>
-            <li>SocialMedia<a href="https://linkedin.com/"></a></li>
-          </ul>
+
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h2 class="panel-title"><a href="https://iterm2.com/">iterm2</a></h2>
+            </div>
+            <div class="panel-body">
+              <div class="col-md-5">
+                <img src="images/iterm.png" class="img-rounded">
+              </div>
+              <div class="col-md-5 col-md-offset-1" >
+                <p>iTerm2 is a replacement for Terminal and the successor to iTerm</p>
+                <p>https://www.iterm2.com/features.html</p>
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h2 class="panel-title"><a href="http://yoember.com/nodejs/the-best-way-to-install-node-js/">Brew (Package Manager)</a></h2>
+            </div>
+            <div class="panel-body">
+              <div class="col-md-5">
+                <img src="images/homebrew.png" class="img-rounded">
+              </div>
+              <div class="col-md-5 col-md-offset-1" >
+                <p>What Does a Package Manager Do?</p>
+                <ol>
+                  <li>Takes a single command as input and figures out which software package you want installed</li>
+                  <li>Downloads the source code of the package (or in Fink's case, the pre-built application itself)</li>
+                  <li>Figures out if any dependencies are present and if so, downloads them as well</li>
+                  <li>Compiles (builds) the dependencies from the source code files and installs them</li>
+                  <li>Builds your requested software</li>
+                  <li>Installs it into the predefined location</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h2 class="panel-title"><a href="http://yoember.com/nodejs/the-best-way-to-install-node-js/">Atom (Text Editor)</a></h2>
+            </div>
+            <div class="panel-body">
+              <div class="col-md-6">
+                <img src="images/atom.png" class="img-rounded">
+              </div>
+              <div class="col-md-5 col-md-offset-1" >
+                <p>Atom is a free and open-source text and source code editor for OS X, Linux, and Windows with support for plug-ins written in Node.js, and embedded Git Control, developed by GitHub. Atom is a desktop application built using web technologies.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h2 class="panel-title">Git & Github<a href="https://git-for-windows.github.io/"></a></h2>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <img src="images/github.jpg" class="img-rounded">
+              </div>
+              <div class="row">
+                <p>Download: <a href="https://git-scm.com">Git</a> & <a href="https://github.com">Github</a></p>
+                <p>'Git' is known as a "distributed" (rather than "centralized") version control system because you can run it locally and disconnected from the Internet, and then "push" your changes to a remote system (such as GitHub) whenever you like.</p>
+              </div>
+            </div>
+          </div>
+
       </div>
     </div>
   `
@@ -152,7 +263,7 @@ app.component('careerAdvice', {
       this.greet = 'Career Advice';
   },
   template: `
-    <div class="jumbotron">
+    <div class="jumbotron" id="careerAdvice">
       <div class="container">
         <h1>{{ $ctrl.greet }}</h1>
           Where is the Bar?
@@ -170,7 +281,7 @@ app.component('references', {
       this.greet = 'References';
   },
   template: `
-    <div class="jumbotron">
+    <div class="jumbotron" id="references">
       <div class="container">
         <h1>{{ $ctrl.greet }}</h1>
         <ul>

e468a0b1071d1147b9baf0e42f91406c0c53af3b
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2016-09-09 14:22:45 +0800

    changed sections into jumbotron

--- app.js
+++ app.js
@@ -60,12 +60,6 @@ app.component('headerComponent', {
     <nav class="navbar navbar-default  navbar-fixed-top">
       <div class="container-fluid">
       <div class="navbar-header">
-         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-           <span class="sr-only">Toggle navigation</span>
-           <span class="icon-bar"></span>
-           <span class="icon-bar"></span>
-           <span class="icon-bar"></span>
-         </button>
          <a class="navbar-brand" href="#">eL</a>
        </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -88,8 +82,12 @@ app.component('discussion', {
       this.greet = 'Discussion Overview';
   },
   template: `
-    <h1>{{ $ctrl.greet }}</h1>
-    <p>To have an open discussion on the latest trends technology and tools used in California</p>
+    <div class="jumbotron">
+      <div class="container">
+        <h1>{{ $ctrl.greet }}</h1>
+        <p>To have an open discussion on the latest trends technology and tools used in California</p>
+      </div>
+    </div>
   `
 });
 
@@ -98,12 +96,16 @@ app.component('careerPaths', {
       this.greet = 'Career Paths';
   },
   template: `
-      <h1>{{ $ctrl.greet }}</h1>
-      <ul>
-        <li>Front End</li>
-        <li>Backend</li>
-        <li>DevOps</li>
-      </ul>
+      <div class="jumbotron">
+        <div class="container">
+          <h1>{{ $ctrl.greet }}</h1>
+          <ul>
+            <li>Front End</li>
+            <li>Backend</li>
+            <li>DevOps</li>
+          </ul>
+        </div>
+      </div>
   `
 });
 
@@ -112,20 +114,13 @@ app.component('checklist', {
       this.greet = 'Critical Checklist';
   },
   template: `
-    <h1>{{ $ctrl.greet }}</h1>
-    <ul>
-      <li>
-        Portfolio (3-5 pieces)
-      </li>
-      <li>
-        Resume
-      </li>
-    </ul>
     <div class="jumbotron">
       <div class="container">
-        <h1>Hello, world!</h1>
-        <p>...</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        <h1>{{ $ctrl.greet }}</h1>
+          <ul>
+            <li>Portfolio (3-5 pieces)</li>
+            <li>Resume</li>
+          </ul>
       </div>
     </div>
   `
@@ -136,32 +131,19 @@ app.component('devTools', {
       this.greet = 'Developers Environment';
   },
   template: `
-    <h1>{{ $ctrl.greet }}</h1>
-    <ul>
-      <li>
-        <a href="https://iterm2.com/">iterm2</a> - terminal tool
-      </li>
-      <li>
-        Brew - used to install node.js
-        <a href="http://yoember.com/nodejs/the-best-way-to-install-node-js/"></a>
-      </li>
-      <li>
-        Atom / Sublime - text editors
-        <a href="https://atom.io"></a>
-      </li>
-      <li>
-        Git
-        <a href="https://git-for-windows.github.io/"></a>
-      </li>
-      <li>
-        Github - repo
-        <a href="https://github.com"></a>
-      </li>
-      <li>
-        SocialMedia
-        <a href="https://linkedin.com/"></a>
-      </li>
-    </ul>
+    <div class="jumbotron">
+      <div class="container">
+        <h1>{{ $ctrl.greet }}</h1>
+          <ul>
+            <li><a href="https://iterm2.com/">iterm2</a> - terminal tool</li>
+            <li>Brew - used to install node.js<a href="http://yoember.com/nodejs/the-best-way-to-install-node-js/"></a></li>
+            <li>Atom / Sublime - text editors<a href="https://atom.io"></a></li>
+            <li>Git<a href="https://git-for-windows.github.io/"></a></li>
+            <li>Github - repo<a href="https://github.com"></a></li>
+            <li>SocialMedia<a href="https://linkedin.com/"></a></li>
+          </ul>
+      </div>
+    </div>
   `
 });
 
@@ -170,11 +152,14 @@ app.component('careerAdvice', {
       this.greet = 'Career Advice';
   },
   template: `
-    <h1>{{ $ctrl.greet }}</h1>
-    Where is the Bar?
-    Latest Trends and Technologies
-    Cool technologies to look into
-
+    <div class="jumbotron">
+      <div class="container">
+        <h1>{{ $ctrl.greet }}</h1>
+          Where is the Bar?
+          Latest Trends and Technologies
+          Cool technologies to look into
+      </div>
+    </div>
   `
 });
 
@@ -183,15 +168,19 @@ app.component('references', {
       this.greet = 'References';
   },
   template: `
-    <h1>{{ $ctrl.greet }}</h1>
-    <ul>
-      <li><h2>Different Career Paths</h2></li>
-      <li><a href="https://www.youtube.com/watch?v=sBzRwzY7G-k&ab_channel=LearnCode.academy">Video</a></li>
-    </ul>
-    <ul>
-      <li><h2>Free Resources</h2></li>
-      <li><a href="https://freecodecamp.com">FreeCodeCamp.com</a></li>
-      <li><a href="https://codeacademy.com">CodeAcademy.com</a></li>
-    </ul>
+    <div class="jumbotron">
+      <div class="container">
+        <h1>{{ $ctrl.greet }}</h1>
+        <ul>
+          <li><h2>Different Career Paths</h2></li>
+          <li><a href="https://www.youtube.com/watch?v=sBzRwzY7G-k&ab_channel=LearnCode.academy">Video</a></li>
+        </ul>
+        <ul>
+          <li><h2>Free Resources</h2></li>
+          <li><a href="https://freecodecamp.com">FreeCodeCamp.com</a></li>
+          <li><a href="https://codeacademy.com">CodeAcademy.com</a></li>
+        </ul>
+      </div>
+    </div>
   `
 });
